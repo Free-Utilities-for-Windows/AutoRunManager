76745f92dc3bd6fc3e8898ac70f4ae9a3a3f639f
Bohdan Harabadzhyu <bohdan_harabadzhyu@outlook.com>
2024-05-28 11:34:45 +0300

    add RegistryHelper...

---
+++ AutoRunManager/RegistryHelper.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.Win32;
+
+namespace AutoRunManager;
+
+public class RegistryHelper
+{
+    public static string[] ReadValueNames(string keyPath)
+    {
+        using (RegistryKey key = Registry.CurrentUser.OpenSubKey(keyPath))
+        {
+            return key?.GetValueNames();
+        }
+    }
+
+    public static object ReadValue(string keyPath, string valueName)
+    {
+        using (RegistryKey key = Registry.CurrentUser.OpenSubKey(keyPath))
+        {
+            return key?.GetValue(valueName);
+        }
+    }
+
+    public static void WriteValue(string keyPath, string valueName, object value)
+    {
+        using (RegistryKey key = Registry.CurrentUser.OpenSubKey(keyPath, true))
+        {
+            key?.SetValue(valueName, value);
+        }
+    }
+
+    public static void DeleteValue(string keyPath, string valueName)
+    {
+        using (RegistryKey key = Registry.CurrentUser.OpenSubKey(keyPath, true))
+        {
+            if (key?.GetValue(valueName) != null)
+            {
+                key.DeleteValue(valueName);
+            }
+        }
+    }
+
+    public static bool KeyExists(string keyPath)
+    {
+        using (RegistryKey key = Registry.CurrentUser.OpenSubKey(keyPath))
+        {
+            return key != null;
+        }
+    }
+
+    public static void DeleteKey(string keyPath)
+    {
+        using (RegistryKey parentKey = Registry.CurrentUser.OpenSubKey(Path.GetDirectoryName(keyPath), true))
+        {
+            parentKey?.DeleteSubKey(Path.GetFileName(keyPath), false);
+        }
+    }
+}
