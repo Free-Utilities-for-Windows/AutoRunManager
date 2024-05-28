3d7ff560ae04e99be47c3c463eed64facfffc5ba
Bohdan Harabadzhyu <bohdan_harabadzhyu@outlook.com>
2024-05-28 11:33:20 +0300

    add Logger and Printer...

---
+++ AutoRunManager/Logger.cs
@@ -0,0 +1,14 @@
+﻿namespace AutoRunManager;
+
+public class Logger
+{
+    private static string logFilePath = "log.txt";
+
+    public static void Log(string message)
+    {
+        using (StreamWriter writer = new StreamWriter(logFilePath, true))
+        {
+            writer.WriteLine($"{DateTime.Now}: {message}");
+        }
+    }
+}
---
+++ AutoRunManager/Printer.cs
@@ -0,0 +1,21 @@
+﻿namespace AutoRunManager;
+
+public static class Printer
+{
+    public static void StartPage()
+    {
+        Console.ForegroundColor = ConsoleColor.DarkMagenta;
+
+        string title = @"
+    _         _        ____              __  __                                   
+   / \  _   _| |_ ___ |  _ \ _   _ _ __ |  \/  | __ _ _ __   __ _  __ _  ___ _ __ 
+  / _ \| | | | __/ _ \| |_) | | | | '_ \| |\/| |/ _` | '_ \ / _` |/ _` |/ _ \ '__|
+ / ___ \ |_| | || (_) |  _ <| |_| | | | | |  | | (_| | | | | (_| | (_| |  __/ |   
+/_/   \_\__,_|\__\___/|_| \_\\__,_|_| |_|_|  |_|\__,_|_| |_|\__,_|\__, |\___|_|   
+                                                                  |___/                                                              
+";
+        Console.WriteLine(title);
+        
+        Console.ResetColor();
+    }
+}
