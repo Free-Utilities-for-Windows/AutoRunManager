7c49cdb781e52c96103a0f1b6703e471edc8e563
Bohdan Harabadzhyu <bohdan_harabadzhyu@outlook.com>
2024-05-28 11:35:43 +0300

    add Validator...

---
+++ AutoRunManager/Validator.cs
@@ -0,0 +1,26 @@
+﻿namespace AutoRunManager;
+
+public class Validator
+{
+    public static bool IsValidFilePath(string path)
+    {
+        try
+        {
+            var firstQuoteIndex = path.IndexOf('"');
+            var lastQuoteIndex = path.LastIndexOf('"');
+
+            if (firstQuoteIndex == -1 || lastQuoteIndex == -1)
+            {
+                return File.Exists(path);
+            }
+
+            var filePath = path.Substring(firstQuoteIndex + 1, lastQuoteIndex - firstQuoteIndex - 1);
+
+            return File.Exists(filePath);
+        }
+        catch
+        {
+            return false;
+        }
+    }
+}
